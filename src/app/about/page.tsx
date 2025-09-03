33388671d1fb73d13635a9377ecfe05df7e60990
Chris Ware <c.p.ware@hotmail.com>
2025-09-03 16:02:44 -0700

    About page intro start

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import AboutPageIntro from "@/components/AboutPage/AboutPageIntro/AboutPageIntro";
+
+export default function AboutPage() {
+  return (
+    <main>
+        <AboutPageIntro />
+    </main>
+  )
+}
---
+++ src/components/AboutPage/AboutPageIntro/AboutPageIntro.tsx
@@ -0,0 +1,28 @@
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
+import styles from "./AboutPageIntro.module.css";
+import SectionIntroii from "@/components/shared/SectionIntroii/SectionIntroii";
+import Image from "next/image";
+import Img1 from "../../../../public/images/specialize.png";
+import Nav from "@/components/shared/Nav/Nav";
+
+export default function AboutPageIntro() {
+  return (
+    <section className={styles.container}>
+      <Nav />
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.imgContainer}>
+            <Image src={Img1} alt='Specialize' fill className={styles.img} />
+          </div>
+          <SectionIntroii title='About us' />
+          <h1 className={styles.heading}>
+            Direct booking <br /> is our #1 sport.
+          </h1>
+          <p className={styles.copy}>
+            We build websites where great service meets zero scheduling drama.
+          </p>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+}
