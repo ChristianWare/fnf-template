90fb93d27158adf8f7e40d41e5adba77e6d6f2e2
Chris Ware <c.p.ware@hotmail.com>
2025-09-06 20:33:55 -0700

    contact page intro done

--- src/components/ContactPage/ContactPageIntro/ContactPageIntro.tsx
+++ src/components/ContactPage/ContactPageIntro/ContactPageIntro.tsx
@@ -3,7 +3,45 @@ import styles from "./ContactPageIntro.module.css";
 import Nav from "@/components/shared/Nav/Nav";
 import Image from "next/image";
 import Img1 from "../../../../public/images/contact.jpg";
+import Img2 from "../../../../public/images/chris.jpg";
 import SectionIntroii from "@/components/shared/SectionIntroii/SectionIntroii";
+import Clock from "@/components/icons/Clock/Clock";
+import Email from "@/components/icons/Email/Email";
+import Instagram from "@/components/icons/Instagram/Instagram";
+import Phone from "@/components/icons/Phone/Phone";
+
+const data = [
+  {
+    id: 1,
+    title: "Chris Ware",
+    desc: "Founder & CEO • chris@fontsandfooters.com",
+    src: Img2,
+  },
+  {
+    id: 2,
+    title: "General Inquiries",
+    desc: "hello@evermind.eco",
+    icon: <Clock className={styles.icon} />,
+  },
+  {
+    id: 3,
+    title: "Press & Media",
+    desc: "press@evermind.eco",
+    icon: <Email className={styles.icon} />,
+  },
+  {
+    id: 4,
+    title: "Instagram",
+    desc: "@evermind.eco",
+    icon: <Instagram className={styles.icon} />,
+  },
+  {
+    id: 5,
+    title: "Customer service",
+    desc: "+32 470 108 222",
+    icon: <Phone className={styles.icon} />,
+  },
+];
 
 export default function ContactPageIntro() {
   return (
@@ -19,7 +57,32 @@ export default function ContactPageIntro() {
           <div className={styles.right}>
             <div className={styles.rightContent}>
               <SectionIntroii title='Contact' />
-              <h1 className={styles.heading}>Let&lsquo;s stay in touch</h1>
+              <h1 className={styles.heading}>
+                {/* Let&lsquo;s stay in touch */}
+                Every meaningful project starts with a conversation.
+              </h1>
+              <div className={styles.mapDataContainer}>
+                {data.map((item) => (
+                  <div key={item.id} className={styles.card}>
+                    <div className={styles.cardLeft}>
+                      <div className={styles.title}>{item.title}</div>
+                      <p className={styles.desc}>{item.desc}</p>
+                    </div>
+                    <div className={styles.cardRight}>
+                      {item.src && (
+                        <Image
+                          src={item.src}
+                          alt='Contact'
+                          className={styles.imgii}
+                        />
+                      )}
+                      {item.icon && (
+                        <div className={styles.iconBox}>{item.icon}</div>
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
