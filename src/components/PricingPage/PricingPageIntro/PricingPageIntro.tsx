cf8ce698db0585661bf9a69a02c81ab2e9e2bfb9
Chris Ware <c.p.ware@hotmail.com>
2025-09-15 10:33:24 -0700

    PricingPageIntro updated

--- src/components/PricingPage/PricingPageIntro/PricingPageIntro.tsx
+++ src/components/PricingPage/PricingPageIntro/PricingPageIntro.tsx
@@ -2,12 +2,24 @@ import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import styles from "./PricingPageIntro.module.css";
 import SectionIntroii from "@/components/shared/SectionIntroii/SectionIntroii";
 import ImageCluster from "@/components/shared/ImageCluster/ImageCluster";
+import Image from "next/image";
+import Img1 from "../../../../public/images/pricingii.png";
+
 
 export default function PricingPageIntro() {
   return (
     <section className={styles.container}>
       <LayoutWrapper>
         <div className={styles.content}>
+          <div className={styles.imgContainer}>
+            <Image
+              src={Img1}
+              alt='thinking'
+              fill
+              className={styles.img}
+              priority
+            />
+          </div>
           <SectionIntroii title='Pricing' />
           <h1 className={styles.heading}>
             From blank page <br /> to booked-solid.
@@ -16,7 +28,7 @@ export default function PricingPageIntro() {
             Choose the engagement that matches your team size, locations, and
             growth goals.
           </p>
-          <ImageCluster color='black' size="small" />
+          <ImageCluster color='black' size='small' />
         </div>
       </LayoutWrapper>
     </section>
