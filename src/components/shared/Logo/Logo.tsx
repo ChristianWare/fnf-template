8bb033ebd8c3677e77fb06edd94369d1ff92f590
Chris Ware <c.p.ware@hotmail.com>
2025-11-18 13:46:31 -0700

    logo updated

--- src/components/shared/Logo/Logo.tsx
+++ src/components/shared/Logo/Logo.tsx
@@ -1,12 +1,15 @@
 import styles from "./Logo.module.css";
 import Link from "next/link";
-import Calendar from "@/components/icons/Calendar/Calendar";
+// import Calendar from "@/components/icons/Calendar/Calendar";
 
 const Logo = () => {
   return (
     <Link href='/' className={styles.logo}>
-      <Calendar className={styles.icon} />
-      <span className={styles.text}>Fonts & Footers</span>
+      {/* <Calendar className={styles.icon} /> */}
+      <div className={styles.icon} />
+      <span className={styles.text1}>Fonts</span>
+      <span className={styles.text2}>&</span>
+      <span className={styles.text3}>Footers</span>
     </Link>
   );
 };
